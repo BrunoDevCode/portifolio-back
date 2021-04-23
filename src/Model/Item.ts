f9e72893b2a1bca4ee032600d898e92fc1a1e5e9
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-04-23 20:43:59 -0300

    feat: Item Model

---
+++ src/Model/Item.ts
@@ -0,0 +1,43 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IItem extends Document {
+  name: string;
+  description: string;
+  quantity: number;
+  cost: number | String;
+  price: number | String;
+  anotherPrice: number | String;
+  userId: string;
+  createdAt: Date;
+}
+
+const ItemModel: Schema<IItem> = new Schema({
+  name: String,
+  description: String,
+  category: String,
+  specialty: String,
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+  cost: Number,
+  price: {
+    type: Number,
+    required: true,
+  },
+  anotherPrice: {
+    type: Number,
+    default: 0,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default model('Item', ItemModel);
