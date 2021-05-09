c8af5c7f6aff7d405195b4c674c371aee82d582c
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-05-09 17:12:04 -0300

    refactor: adding create user useCase

---
+++ src/modules/User/useCase/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { MongooseUserRepository } from '../../repositories/implementations/MongooseUserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserUseCase {
+  constructor(private userRepository: MongooseUserRepository) {}
+
+  async execute({ name, email, password }: IRequest): Promise<string> {
+    const userExists = this.userRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new Error('User already exists!');
+    }
+
+    const id = this.userRepository.register({ name, email, password });
+
+    return id;
+  }
+}
+
+export { CreateUserUseCase };
