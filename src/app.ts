086be7c8dab64f7139d2021ad53f2fde5b6fc53d
Bruno Henrique <brunohenriquesantos272@gmail.com>
2021-06-03 00:17:40 -0300

    build: add error handling, TSyringe, db file and reflect-metadata

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { routes } from './routes';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import { MONGO_URI } from './config/env';
-import { AppError } from './config/AppError';
+import 'reflect-metadata';
 
-connect(MONGO_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-}).then(() => console.log('> DB connection sucessful'));
+import cors from 'cors';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { AppError } from './errors/AppError';
+import { router } from './routes';
+
+import './database';
+
+import './shared/container';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(router);
 
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
